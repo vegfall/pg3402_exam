2ad0a69e98e077459bd47b86cc8a82790827baa8
vegfall <vefa003@student.kristiania.no>
2024-12-06 22:25:36 +0100

    Adding forgotten files

---
+++ app/src/application/application.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Application() {
+  return (
+    <>
+      <h1>Hello World!</h1>
+    </>
+  );
+}
---
+++ app/src/main.tsx
@@ -0,0 +1,6 @@
+import React from "react";
+import { createRoot } from "react-dom/client";
+import Application from "./application/application";
+
+const root = createRoot(document.getElementById("root")!);
+root.render(<Application />);
