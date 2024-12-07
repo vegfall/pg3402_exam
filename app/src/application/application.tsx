c9509590bbdf4562fcaff6bbfcf292a50fc015ba
vegfall <vefa003@student.kristiania.no>
2024-12-07 15:36:17 +0100

    Added api request functionality to frontend. Added corsconfig to fix browser restrictions. Not implemented with github pages yet

--- app/src/application/application.tsx
+++ app/src/application/application.tsx
@@ -1,9 +1,25 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 
 export default function Application() {
+  const [message, setMessage] = useState<string>("Getting message...");
+
+  useEffect(() => {
+    axios
+      .get("http://127.0.0.1:8080/api/test")
+      .then((response) => {
+        setMessage(response.data);
+      })
+      .catch((error) => {
+        console.error("Error fetching data:", error);
+        setMessage("Error fetching message");
+      });
+  }, []);
+
   return (
     <>
-      <h1>Hello World!</h1>
+      <h1>Message from backend:</h1>
+      <p>{message}</p>
     </>
   );
 }
