2260670961273d179631806e0bddad94a3fdc9f8
vegfall <vefa003@student.kristiania.no>
2024-12-16 09:41:33 +0100

    Added properties and a flyway script

---
+++ quiz/src/main/resources/db/migration/V1__Initialize_Quiz_Schema.sql
@@ -0,0 +1,8 @@
+CREATE TABLE sessions (
+      session_id BIGINT AUTO_INCREMENT PRIMARY KEY,
+      session_key VARCHAR(255) NOT NULL,
+      theme VARCHAR(255) NOT NULL,
+      number_of_alternatives INT NOT NULL,
+      username VARCHAR(255) NOT NULL,
+      current_question_key INT NOT NULL
+);
