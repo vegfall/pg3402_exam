fe2e50a0dadc3bff1753759d40be55271c3ccc42
vegfall <vefa003@student.kristiania.no>
2024-12-16 09:43:27 +0100

    Added entity for database

---
+++ quiz/src/main/java/com/quiz/quiz/entity/SessionEntity.java
@@ -0,0 +1,23 @@
+package com.quiz.quiz.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "sessions")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class SessionEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long sessionId;
+
+    private String sessionKey;
+    private String theme;
+    private int numberOfAlternatives;
+    private String username;
+    private int currentQuestionKey;
+}
