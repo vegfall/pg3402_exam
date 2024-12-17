885abacc2b1f2a2cad0323064dbd049c3b1f021f
vegfall <vefa003@student.kristiania.no>
2024-12-17 12:13:22 +0100

    Updated AMQP for Question service

--- question/src/main/java/com/quiz/question/config/AMQPConfig.java
+++ question/src/main/java/com/quiz/question/config/AMQPConfig.java
@@ -1,7 +1,6 @@
 package com.quiz.question.config;
 
-import org.springframework.amqp.core.ExchangeBuilder;
-import org.springframework.amqp.core.TopicExchange;
+import org.springframework.amqp.core.*;
 import org.springframework.amqp.rabbit.connection.ConnectionFactory;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
@@ -13,13 +12,36 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class AMQPConfig {
     @Value("${amqp.exchange.name}")
-    private String exchangeName;
+    private String resultExchangeName;
+
+    @Value("${amqp.ai.exchange.name}")
+    private String aiExchangeName;
+
+    @Value("${amqp.queue.ai.response}")
+    private String aiResponseQueue;
 
     @Bean
     public TopicExchange resultExchange() {
-        return ExchangeBuilder.topicExchange(exchangeName)
+        return ExchangeBuilder.topicExchange(resultExchangeName)
                 .durable(true)
                 .build();
+    }
+
+    @Bean
+    public TopicExchange aiExchange() {
+        return ExchangeBuilder.topicExchange(aiExchangeName)
+                .durable(true)
+                .build();
+    }
+
+    @Bean
+    public Queue aiResponseQueue() {
+        return QueueBuilder.durable(aiResponseQueue).build();
+    }
+
+    @Bean
+    public Binding aiResponseBinding(Queue aiResponseQueue, TopicExchange aiExchange) {
+        return BindingBuilder.bind(aiResponseQueue).to(aiExchange).with("ai.queue.response");
     }
 
     @Bean
