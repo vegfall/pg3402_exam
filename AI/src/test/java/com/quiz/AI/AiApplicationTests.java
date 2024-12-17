6434f5306a3c484ec6f4153d2ef3e1dfc05ebbaf
vegfall <vefa003@student.kristiania.no>
2024-12-17 09:48:06 +0100

    Added ai service

---
+++ AI/src/test/java/com/quiz/AI/AiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.quiz.AI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
